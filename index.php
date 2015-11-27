14e25cda663e1ce705134b0b8e460f1f0e60fe48
jasonrkirk@gmail.com <jasonrkirk@gmail.com>
2015-11-27 00:57:48 +0000

    Adding more than basic

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+include 'connection.php';
 
-echo $_SERVER['REMOTE_ADDR']
+echo $_SERVER['REMOTE_ADDR'];
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$tempish = $_GET["owner"];
+$sql = "INSERT INTO ips (ip, owner, location, sublocation)
+VALUES ('$ip_address','";
+$sql .= $_GET["owner"];
+$sql .= "','";
+$sql .= $_GET["location"];
+$sql .= "','";
+$sql .= $_GET["sublocation"];
+$sql .= "')";
+
+if ($dbc->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$dbc->close();
 
 ?>
